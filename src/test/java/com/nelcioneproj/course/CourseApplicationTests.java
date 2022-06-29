9f03e1210e77f7664a063aa42b0ee0140e622f3b
Nelcione Dias <nelcionedias@gmail.com>
2022-06-29 11:28:36 -0300

    Project created

---
+++ src/test/java/com/nelcioneproj/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelcioneproj.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
