5f10b54694ee87ba7d9901db4b69826a006fda52
Nelcione Dias <nelcionedias@gmail.com>
2022-07-03 16:16:36 -0300

    Exception handling - delete

--- src/main/java/com/nelcioneproj/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelcioneproj/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelcioneproj.course.services.exceptions.DatabaseException;
 import com.nelcioneproj.course.services.exceptions.ResourceNotFoundException;
 
 //tratamento personalizado da ResourceExceptionHandler
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resorce not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class) // fala que o método vai interceptar qualquer exception lançada
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/nelcioneproj/course/services/UserService.java
+++ src/main/java/com/nelcioneproj/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nelcioneproj.course.entities.User;
 import com.nelcioneproj.course.repositories.UserRepository;
+import com.nelcioneproj.course.services.exceptions.DatabaseException;
 import com.nelcioneproj.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,7 +36,14 @@ public class UserService {
 	
 	//delete user
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	//update user
