211edbd5459fc118c0d97ab63fabd274524881a6
Nelcione Dias <nelcionedias@gmail.com>
2022-07-03 14:18:06 -0300

    Payment one-to-one association

--- src/main/java/com/nelcioneproj/course/config/TestConfig.java
+++ src/main/java/com/nelcioneproj/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nelcioneproj.course.entities.Category;
 import com.nelcioneproj.course.entities.Order;
 import com.nelcioneproj.course.entities.OrderItem;
+import com.nelcioneproj.course.entities.Payment;
 import com.nelcioneproj.course.entities.Product;
 import com.nelcioneproj.course.entities.User;
 import com.nelcioneproj.course.entities.enums.OrderStatus;
@@ -84,6 +85,12 @@ public class TestConfig implements CommandLineRunner{ //instancia o banco de dad
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2022-06-29T18:05:07Z") , o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
